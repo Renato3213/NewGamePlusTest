a0044e7f94142b0a71f0d27290b166fa96b11aa8
Renato3213 <112446646+Renato3213@users.noreply.github.com>
2025-07-25 22:53:11 -0300

    Player Basic Movement

---
+++ Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    private BoxCollider2D _boxCol;
+
+    #region Movement Session
+
+    [SerializeField] float _moveSpeed;
+    private Vector2 _moveDirection;
+
+    #endregion
+
+
+    #region Jump Session
+
+    [SerializeField] LayerMask _groundLayer;
+
+    bool _readyToJump = true;
+
+    [SerializeField] float _jumpStrenght = 10f;
+
+    [SerializeField] float _jumpCooldown = 0.1f;
+
+    bool _jumpBuffered;
+    [SerializeField] float _jumpBufferTime = 0.15f;
+
+    [SerializeField] float _coyoteTime;
+    float _coyoteTimer;
+
+    bool _grounded;
+
+    #endregion
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        _boxCol = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _grounded = Physics2D.BoxCast(transform.position, _boxCol.size, 0, Vector2.down, _boxCol.bounds.extents.y + 0.2f, _groundLayer);
+
+        HandleInput();
+
+        HandleCoyoteTime();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleCoyoteTime()
+    {
+        if (_grounded && _readyToJump)
+            _coyoteTimer = _coyoteTime;
+        else if (_coyoteTimer > 0)
+            _coyoteTimer -= Time.deltaTime;
+    }
+
+    void HandleInput()
+    {
+        _moveDirection.x = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartJumpBuffer();
+        }
+
+
+        //when to jump
+        if (_jumpBuffered && _readyToJump && (_grounded || _coyoteTimer > 0))
+        {
+            _coyoteTimer = 0;
+            //_jumpBufferTimer = 0;
+            _jumpBuffered = false;
+            _readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), _jumpCooldown);
+        }
+    }
+
+
+    void HandleMovement()
+    {
+        _rb.linearVelocityX = _moveDirection.x * _moveSpeed;
+    }
+    void Jump()
+    {
+        _rb.linearVelocityY = 0;
+        _rb.AddForce(Vector2.up * _jumpStrenght, ForceMode2D.Impulse);
+    }
+
+    void ResetJump()
+    {
+        _readyToJump = true;
+    }
+
+    void StartJumpBuffer()
+    {
+        _jumpBuffered = true;
+        Invoke(nameof(EndJumpBuffer), _jumpBufferTime);
+    }
+
+    void EndJumpBuffer()
+    {
+        _jumpBuffered = false;
+    }
+}
