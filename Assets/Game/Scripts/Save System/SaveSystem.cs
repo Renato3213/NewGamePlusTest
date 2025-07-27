5ab0d69db34a7b0b77d54903ea5acac7d154e745
Renato3213 <112446646+Renato3213@users.noreply.github.com>
2025-07-26 23:16:53 -0300

    save system

---
+++ Assets/Game/Scripts/Save System/SaveSystem.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.IO;
+
+public class SaveSystem
+{
+    private static SaveData _saveData = new SaveData();
+
+    [System.Serializable]
+    public struct SaveData
+    {
+        public PlayerSaveData PlayerData;
+    }
+
+    public static string SaveFileName()
+    {
+        string saveFile = Application.persistentDataPath + "/save" + ".save";
+        return saveFile;
+    }
+
+    public static void Save()
+    {
+        HandleSaveData();
+
+        File.WriteAllText(SaveFileName(), JsonUtility.ToJson(_saveData, true));
+    }
+
+    private static void HandleSaveData()
+    {
+        GameManager.Instance.Player.Save(ref _saveData.PlayerData);
+    }
+
+    public static void Load()
+    {
+        string saveContent = File.ReadAllText(SaveFileName());
+
+        _saveData = JsonUtility.FromJson<SaveData>(saveContent);
+        HandleLoadData();
+    }
+
+    private static void HandleLoadData()
+    {
+        GameManager.Instance.Player.Load(_saveData.PlayerData);
+    }
+}
