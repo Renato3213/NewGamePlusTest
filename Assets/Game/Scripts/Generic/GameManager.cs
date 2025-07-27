748f79239525fed0edbf483b41d1de0bd35469ee
Renato3213 <112446646+Renato3213@users.noreply.github.com>
2025-07-26 23:17:30 -0300

    Game manager

--- Assets/Game/Scripts/Generic/GameManager.cs
+++ Assets/Game/Scripts/Generic/GameManager.cs
@@ -1,16 +1,58 @@
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviour //game manager is set to run before anything else
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private static GameManager instance;
+
+    public static GameManager Instance
     {
-        
+        get
+        {
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+            {
+                return null;
+            }
+
+            if(instance == null)
+            {
+                Instantiate(Resources.Load<GameManager>("GameManager"));
+            }
+#endif
+            return instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public PlayerController Player { get; set; }
+
+
+    public void Awake()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Door.OnDoorOpened += HandleDoor;
     }
+
+    void HandleDoor(bool isWinCondition)
+    {
+        if (isWinCondition)
+        {
+            //win
+        }
+
+        else
+        {
+            //change scene
+        }
+    }
+
+
 }
