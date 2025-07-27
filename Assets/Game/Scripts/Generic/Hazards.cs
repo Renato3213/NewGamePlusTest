33faf3eb569f23eca12f2a5d16d848a692260a91
Renato3213 <112446646+Renato3213@users.noreply.github.com>
2025-07-27 14:54:30 -0300

    Game scene save

--- Assets/Game/Scripts/Generic/Hazards.cs
+++ Assets/Game/Scripts/Generic/Hazards.cs
@@ -5,11 +5,9 @@ public class Hazards : MonoBehaviour
     public int damage = 1;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("a");
         Damageable damageable;
         if(collision.TryGetComponent<Damageable>(out damageable))
         {
-        Debug.Log("b");
             damageable.TakeDamage(damage, 6f);
         }
     }
